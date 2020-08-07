1846827713247ef767e8b948f221c12a910e753f
rickytorrez <torrez.ricky@ymail.com>
2020-08-07 11:34:10 -0400

    add /blog and markdown support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default () => {
+  return (
+    <>
+      Hello world!
+      <p>
+        <Link to="/blog">View Blog</Link>
+      </p>
+    </>
+  )
 }
